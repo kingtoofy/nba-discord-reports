7a75981571ccfe3b1fe902a2c64fbe55fe00e2ed
kingtoofy <kingtoofy@gmail.com>
2025-12-18 11:53:02 -0800

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,9 @@
+import os
+from report_writer import picks_report, daily_report
+from send_discord import send
 
+PICKS_WEBHOOK = os.getenv("DISCORD_PICKS_WEBHOOK")
+REPORT_WEBHOOK = os.getenv("DISCORD_REPORT_WEBHOOK")
+
+send(PICKS_WEBHOOK, picks_report())
+send(REPORT_WEBHOOK, daily_report())
