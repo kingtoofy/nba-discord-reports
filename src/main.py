8d0d7edf340663c9653a21db431285e04211e0ba
kingtoofy <kingtoofy@gmail.com>
2025-12-18 12:46:26 -0800

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
 from src.report_writer import picks_report, daily_report
 from src.send_discord import send  # your existing Discord webhook sender
 
-PICKS_WEBHOOK = "YOUR_WEBHOOK_URL_FOR_NBA_DAILY_PICKS"
-REPORT_WEBHOOK = "YOUR_WEBHOOK_URL_FOR_NBA_DAILY_REPORT"
+PICKS_WEBHOOK = "https://discord.com/api/webhooks/1451285690158546965/mJnVnJ8GDmkWFcU8S-nHoSyJL0aWSri-erfqw-3pekFy5LOZek0QK7pXF65DdHucspn1"
+REPORT_WEBHOOK = "https://discord.com/api/webhooks/1451299023335063562/7M3u4hJ_vaJ0Q8LweCojTvX2halUGJAGrUE1Wv1LMb28Fwb3wB7mkuJOMAuw6nThmKKA"
 
 # Send top picks
 send(PICKS_WEBHOOK, picks_report())
